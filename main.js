9aaa97d26af455294e2aeead1229bbaf6b3d1da7
Michael Salvati <print@3daddict.com>
2018-08-22 17:31:43 -0700

    Create a click handler for td's v2

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 $(document).ready(initializeApp);
 
-function initializeApp () {
-    // $(".cell").click(cellClick);
+function initializeApp() {
+    drawBoard();  
+    $("td").click(clickHandler);
 
-    drawBoard()
 }
 
-
+//Global Variables
+var currentPlayer = 0;
+var player1Selection = [];
+var player2Selection = [];
 
 
 // Create board dynamically on the dom
-function drawBoard () {
+function drawBoard() {
     var counter = 1;
     for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
         var row = $("<tr>")
         for (var colIndex = 0; colIndex < 3; colIndex++) {
-            row.append('<td>');
+            var square = $("<td>").attr('id', counter++);
+            row.attr('row', rowIndex);
+            square.attr('col', colIndex);
+            row.append(square);
         }
         $('#game').append(row);
     }
+}
 
+function clickHandler() {
+    if(currentPlayer === 0) {
+        $(this).text('X');
+        currentPlayer = 1;
+    } else {
+        $(this).text('O');
+        currentPlayer = 0;
+    }
 }
